e5db100bc15a28143f9f389fe6a381545dea29b1
Matt <matthew7j@aol.com>
2017-10-09 20:51:47 -0400

    Add project files.

---
+++ StockAnalysis/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+
+namespace StockAnalysis
+{
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BtnFileSelect_Click(object sender, RoutedEventArgs e) {
+
+            var fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+
+            switch (result) {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    TxtBlkFIleSelect.Text = file;
+                    TxtBlkFIleSelect.ToolTip = "Directory of PDFs for analysis";
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    TxtBlkFIleSelect.Text = null;
+                    TxtBlkFIleSelect.ToolTip = null;
+                    break;
+            }
+        }
+    }
+}
